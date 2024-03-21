4d496e16aca0bbd6636116e1b562e23cc6b25906
Zavier Hopson <zo.hopson94@gmail.com>
2024-03-21 16:36:13 -0500

    renders filtered creatures by userId, navbar button works

--- src/components/creatures/UserCreature.jsx
+++ src/components/creatures/UserCreature.jsx
@@ -1,18 +1,53 @@
 import React from "react";
-import { deleteCreature } from "../../services/creatureService";
+import { filterUserCreatures } from "../../services/creatureService";
 
 export const UserCreature = ({ currentUser }) => {
-  const [showUserCreatures, setShowUserCreatures] = React.useState([]);
+  const [userCreatures, setUserCreatures] = React.useState([]);
 
   React.useEffect(() => {
-    if (currentUser.id == creature.userId) {
-    }
-  });
+    filterUserCreatures(currentUser.id).then((creaturesArray) => {
+      setUserCreatures(creaturesArray);
+    });
+  }, [currentUser]);
 
   return (
-    <section>
-      <div className="user-creature"></div>
-    </section>
+    <article className="creatures">
+      {userCreatures.map((creature) => {
+        return (
+          <section className="creature" key={creature.id}>
+            <header className="creature-info">{creature?.name}</header>
+            <h3>Description</h3>
+            <div>{creature?.description}</div>
+            <h3>Diet</h3>
+            <div>{creature?.diet}</div>
+            <h3>Country of Origin</h3>
+            <div>{creature?.country.name}</div>
+            <h3>Category</h3>
+            <div>{creature?.category.category}</div>
+            {creature.userId === currentUser.id ? (
+              <div>
+                <button
+                  onClick={() => {
+                    deleteCreature(creature);
+                  }}
+                >
+                  Terminate
+                </button>
+                <button
+                  onClick={() => {
+                    navigate(`../edit/${creature.id}`);
+                  }}
+                >
+                  Edit
+                </button>
+              </div>
+            ) : (
+              ""
+            )}
+          </section>
+        );
+      })}
+    </article>
   );
 };
 
