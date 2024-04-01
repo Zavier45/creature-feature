de0ebcfff3f2583d871fb813462ede120194d9a6
Zavier Hopson <zo.hopson94@gmail.com>
2024-04-01 10:50:47 -0500

    ??

--- src/components/creatures/UserCreature.jsx
+++ src/components/creatures/UserCreature.jsx
@@ -37,6 +37,8 @@ export const UserCreature = ({ currentUser }) => {
             <div>{creature?.country.name}</div>
             <h3>Category</h3>
             <div>{creature?.category.category}</div>
+            <h3>Tales</h3>
+            <div>{creature?.tales}</div>
             <div>
               <img
                 src={creature.image}
