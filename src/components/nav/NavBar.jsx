46bece9513b2b16a02e541d2ac89f1ca90fc439e
Zavier Hopson <zo.hopson94@gmail.com>
2024-04-01 10:59:56 -0500

    added profile link

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -4,36 +4,37 @@ import "./NavBar.css";
 export const NavBar = () => {
   return (
     <div className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/creatures">All Creatures</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/user-creature">My Creatures</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/new-creature">New Creature</Link>
-        </li>
-        {localStorage.getItem("creature_user") ? (
-          <li className="navbar-item navbar-logout">
-            <Link
-              className="navbar-link"
-              to="/login"
-              onClick={() => {
-                localStorage.removeItem("creature_user");
-                Navigate("/", { replace: true });
-              }}
-            >
-              Logout
-            </Link>
-          </li>
-        ) : (
-          ""
-        )}
-      </ul>
+      <span className="navbar-item">
+        <Link to="/">Home</Link>
+      </span>
+      <span className="navbar-item">
+        <Link to="/creatures">All Creatures</Link>
+      </span>
+      <span className="navbar-item">
+        <Link to="/user-creature">My Creatures</Link>
+      </span>
+      <span className="navbar-item">
+        <Link to="/new-creature">New Creature</Link>
+      </span>
+      <span className="navbar-item">
+        <Link to="/user-profile">Profile</Link>
+      </span>
+      {localStorage.getItem("creature_user") ? (
+        <span className="navbar-item navbar-logout">
+          <Link
+            className="navbar-link"
+            to="/login"
+            onClick={() => {
+              localStorage.removeItem("creature_user");
+              Navigate("/", { replace: true });
+            }}
+          >
+            Logout
+          </Link>
+        </span>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
